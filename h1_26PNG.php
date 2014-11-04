a308cceb97813774561ebe4194ec29443616b008
jose Trincado <josenass22@gmail.com>
2014-11-04 10:32:22 +0100

    ej26 captcha jpg

--- h1_26PNG.php
+++ h1_26PNG.php
@@ -1,13 +1,34 @@
 <?php
-header("Content-type: image/png");
- 
-$img1 = imagecreatefromgif("img/fondo.png");
-$fuente = "LUCON.TTF";
-$color = imagecolorallocate($img1,0,0,0);
- 
-imagettftext($img1,30,0,28,50,$color,$fuente, "MEXICO");
- 
-imagegif($img1);
- 
-imagedestroy($img1);
+header('Content-Type: image/jpeg');
+header("Content-Disposition:inline ; filename=img/fondo.jpeg");
+
+$fuente = "OpenSans-Regular.ttf";
+$texto = "gato";//$_GET['text'];
+
+//$im = imagecreatetruecolor(300, 100);
+$im = imagecreatefromjpeg('img/fondo.jpeg');
+
+$color = imagecolorallocate($im, 255, 255, 255);
+
+//dibuja elipse
+//imagearc($im, 13, 13, 13 , 13, 13, 13, $negro);
+//crear imagen rectángulo
+//imagefilledrectangle($im, 0, 0, 299, 99, $rojo);
+
+//tamaño de imagen
+$width= imagesx($im);
+$heigth= imagesy($im);
+//tamaño de patron
+$x=rand(0,$width/3);
+$y=rand(1,$heigth/3);
+
+for($i=0;$i<strlen($texto);$i++){
+	$x=$x+rand(12,30);
+	$y=$y+rand(5,20);
+	imagefttext($im, 20, rand(0,359), $x, $y, $color, $fuente, $texto[$i]);
+}
+
+// Imprimir la imagen al navegador
+imagepng($im);
+imagedestroy($im);
 ?>
