fefa2ae7d9bc485f5b277e7bb700b4c83b787044
jose Trincado <josenass22@gmail.com>
2014-12-18 11:42:51 +0100

    cookies exercise 35

--- 33-34_SQLRELACIONAL/index35.php
+++ 33-34_SQLRELACIONAL/index35.php
@@ -7,25 +7,44 @@ require('funciones.php');
 ?>
 <style>
 body {
-    font-family: 'OpenSans-Regular.ttf';
-    background: linear-gradient(45deg, rgba(0,0,0,0.65) 0%,rgba(0,0,0,0) 100%); /* W3C */
-    background-image:url('../img/backindex.jpg');
-    background-size: 100% 100%;
-    color: #000;
-    font-size: 11px;
+    background-image:url(<?php echo $_COOKIE['background'] ?>);
     text-align: left;
+}
+.cartel{
+    width: 30%;
+    height: 400px;
+    border-bottom: 2px groove <?php echo  $_COOKIE['box-color'] ?>;
+    margin: 20px 15px;
+    box-shadow: 0px 10px 20px <?php echo $_COOKIE['box-color']?>;
+    float: left;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    background-size: 100% 100%;
+}
+.cartel > h1 {
+    margin-top: 245px;
+    margin-left: inherit;
+    border-radius-right: none; 
+}
+.cartel > *{
+    border-radius: 5px;
+    color: <?php echo $_COOKIE['color']?>;
+    padding: 7px;
+    background-color: rgba(255,255,255,0.8);
+    margin-left: 4px;
+    box-shadow: 2px 1px 9px #CCC;
 }
 </style>
 </head>
 <body>
 <?php
 /*
-Requisitos de inserción
-Realizar un formulario de petición de datos para insertar una serie de películas, actores y tipos.
-Cuando introduzcamos las películas el tipo de película nos lo presentara una lista desplegable. (terror, humor, etc)
-Cualquier tipo de error que se produjese en la base de datos, tanto de conexión, como de inserción deben ser comunicados a los usuarios con error.php
+use to 35 cookie files
 */
-	
+if(!isset($_COOKIE['Fecha']) && !isset($_COOKIE['idioma']) && !isset($_COOKIE['background']) && !isset($_COOKIE['color'])  && !isset($_COOKIE['box-color'])){
+	echo "<div class='alert'>Debe activar su configuración Redirigiendo ...</div>";
+	echo '<META http-equiv="refresh" content="3;URL=../35_COOKIES/index.php">';
+}else{
 	if(isset($_GET['edit'])){
 		echo '<div class="formLS">
 					<form action='.$_SERVER['PHP_SELF'].' method=\'POST\' enctype=\'multipart/form-data\'>
@@ -34,15 +53,7 @@ Cualquier tipo de error que se produjese en la base de datos, tanto de conexión
 					    <h3>Actor: <select type="text" name="actor" style="margin:auto;">';
 					  	roption($mysqli,'personal');
 					  	echo '</select></h3>
-					    <h3>Director: <select typewidth: 30%;
-height: 400px;
-border-bottom: 2px groove lightgreen;
-margin: 20px 15px;
-box-shadow: 0px 10px 20px lightgray;
-float: left;
-overflow-y: hidden;
-overflow-x: hidden;
-background-size: 100% 100%;="text" name="director" style="margin:auto;">';
+					    <h3>Director: <select type="text" name="director" style="margin:auto;">';
 					    roption($mysqli,'personal');
 					    echo '</select></h3>
 					    <h3>Tipo: <select type="text" name="tipo" style="margin:auto;">';
@@ -53,7 +64,7 @@ background-size: 100% 100%;="text" name="director" style="margin:auto;">';
 					</form>
 			   </div>';
 	}elseif (isset($_POST['titulo']) && isset($_POST['actor']) && isset($_POST['director']) && isset($_POST['tipo']) && isset($_POST['año'])){	
-		echo '<div style="display:block;text-align: end;">';
+		echo '<div style="display:block;text-align: end;"><span class="notifi"> Fecha:'.$_COOKIE['Fecha'].' Idioma:'.$_COOKIE['idioma'].'</span>';
 			try{
 				createfilm($mysqli, $_POST['titulo'], $_POST['tipo'], UpPhoto('../cartelera/'), $_POST['actor'], $_POST['director'], $_POST['año']);
 			}catch(Exception $e){
@@ -68,7 +79,8 @@ background-size: 100% 100%;="text" name="director" style="margin:auto;">';
 			echo'<div class="alert">'.$e->getMessage().'</div>';
 		}
 	}else{
-		echo '<div style="display:block;text-align: end;"><a href="index.php?edit=true" class=\'btn\'>Editar Peliculas</a>'
+		echo '<div style="display:block;text-align: end;"><span class="notifi"> Fecha:'.$_COOKIE['Fecha'].' Idioma:'.$_COOKIE['idioma'].'</span>'
+		.'<a href="index.php?edit=true" class=\'btn\'>Editar Peliculas</a>'
 		.'<a href="crear.php" class=\'btn\' >Crear bbdd</a></div>';
 		try{
 			mostrar($mysqli);
@@ -76,6 +88,10 @@ background-size: 100% 100%;="text" name="director" style="margin:auto;">';
 			echo'<div class="alert">'.$e->getMessage().'</div>';
 		}
 	}
+	foreach ($_COOKIE as $key -> $value) {
+		unset($_COOKIE[$key]);
+	}
+}
 ?>
 </body>
 </html>
--- 35_COOKIES/index.php
+++ 35_COOKIES/index.php
@@ -1,5 +1,6 @@
 <!doctype html>
-<head><meta http-equiv="Content-Type" content="charset=utf-8">
+<head>
+<meta http-equiv="Content-Type" content="charset=utf-8">
 <?php
 /*
 Antes de introducir las películas nos aparece una pantalla de configuración de nuestro sitio web,
@@ -10,21 +11,53 @@ Este fichero será
 */
 //bool setcookie ( string $nombre [, string $valor [, int $expira = 0 [, string $ruta [, string $dominio [, bool $segura = false [, bool $httponly = false ]]]]]] );
 include('../style/style.css');
+if (isset($_POST['color']) && isset($_POST['box-color']) && isset($_POST['background'])){
+	setcookie('Fecha',date('Y-m-d H:i:s'), time()+60*60,'/');
+	setcookie('idioma','ESP',time()+60*60,'/');
+	setcookie('color',$_POST['color'],time()+60*60,'/');
+	setcookie('box-color',$_POST['box-color'],time()+60*60,'/');
+	setcookie('background',$_POST['background'],time()+60*60,'/');
+}
 ?>
 </head>
 <body>
-	<div class='div'>
+	<div class="formLS">
+		<h2>Configuración de cartelera</h2>
+		<form action=<?php echo $_SERVER['PHP_SELF']; ?> method='POST' enctype='multipart/form-data'>
+		    <h3>Color de texto: 
+		    <select type="text" name="color" style="margin:auto;">';
+			    <option value="orange">naranja</option>
+			    <option value="green">verde</option>
+			    <option value="grey">gris</option>
+		    </select></h3>
+		    <br>
+		    <h3>Color de carteles: <select type="text" name="box-color" style="margin:auto;">';
+			    <option value="orange">naranja</option>
+			    <option value="green">verde</option>
+			    <option value="grey">gris</option>
+		    </select></h3>
+		    <h3>Fondo: <select type="text" name="background" style="margin:auto;">';
+			    <option value="../img/backindex.jpg">fondo 1</option>
+			    <option value="../img/backindex2.jpg">fondo 2</option>
+			    <option value="../img/backindex3.jpg">fondo 3</option>
+		    </select></h3>
+		    <!-- <h3>Estilo: <select type="text" name="style" style="margin:auto;">';
+			    <option value="1">estilo 1</option>
+			    <option value="2">estilo 2</option>
+			    <option value="3">estilo 3</option>
+		    </select></h3> -->
+		    <input type="submit" value="Enviar" class="sb" style="width:300px"/>
+		</form>
+	</div>
 <?php
-setcookie('Fecha',date('Y-m-d H:i:s'));
-setcookie('preferencias[idioma]','español');
-setcookie('preferencias[fondo]','rojo');
-var_dump($_COOKIE);
-if(!isset($_COOKIE['Fecha'])){
-	echo 'No se han activado cookies';
-}else{
-	echo 'Gracias por activar las cookies';
+if (isset($_POST['color']) && isset($_POST['box-color']) && isset($_POST['background'])){
+	echo '<META http-equiv="refresh" content="0.8;URL=../33-34_SQLRELACIONAL/index35.php">';
 }
-echo "<br><a class='btn' href=$_SERVER[PHP_SELF]>Refresh</a>"
+if(!isset($_COOKIE['Fecha']) && !isset($_POST['color']) ){
+	echo '<div class="alert">No se han activado cookies</div>';
+}else{
+	echo '<div class="notifi">Gracias por activar las cookies</div>';
+}
 ?>
 	</div>
 </body>
